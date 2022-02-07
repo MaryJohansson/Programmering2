3577f7a1827e96c9933b9a8d123ba6513d54d293
Ida <99015842+ida1oo@users.noreply.github.com>
2022-02-07 09:08:42 +0100

    idas testfil

--- Idas_Testfil.py
+++ Idas_Testfil.py
@@ -1,1 +1,10 @@
-print("Testar")
+class Organization:
+
+    def __init__(self):
+        """Varje organisation ska ha dessa attribut:
+        Id, Namn, Skolor
+        Datatyper: String, String, [School]"""
+        pass
+
+    def add_school(self):
+        pass
