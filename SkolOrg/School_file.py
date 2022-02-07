de3bbe9fa45c1e00d3cbc5b931a9ac4c953bf31f
MaryJohansson <99015835+MaryJohansson@users.noreply.github.com>
2022-02-07 09:10:27 +0100

    test

--- SkolOrg/School_file.py
+++ SkolOrg/School_file.py
@@ -1,3 +1,21 @@
-print("Hej Mary, hur går det?")
-print("hEJ ida")
-print("hej kacki")
+class School:
+
+    def __init__(self):
+        ID, Name, Adress, Student
+        Datatyper: String, String, String, [Student]"""
+        pass
+
+    def add_student(self):
+        """Lägg till en elev"""
+        pass
+
+    def remove_student(self):
+        """Ta bort en specifik elev"""
+        pass
+
+    def remove_class(self):
+        """Ta bort alla elever från en specifik klass"""
+        pass
+
+    def change_address(self):
+        pass
