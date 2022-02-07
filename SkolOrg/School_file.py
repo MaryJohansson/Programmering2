d76811c068c383d540c603fada78b4c1d712bd99
MaryJohansson <99015835+MaryJohansson@users.noreply.github.com>
2022-02-07 09:41:45 +0100

    hej

--- SkolOrg/School_file.py
+++ SkolOrg/School_file.py
@@ -1,31 +1,14 @@
 class School:
 
-    def __init__(self,Id, Name, Adress, Student):
-        self.Id = Id
-        self.Name = Name
-        self.Adress = Adress
-        self.Student = Student
+    def __init__(self,id, name, adress, *programs):
+        self.id = id
+        self.name = name
+        self.adress = adress
+        self.programs = programs
 
         def __str__(self):
-            return f"Id:{self.Id} Name:{self.Name} Adress:{self.Adress}  Student:{self.student}"
+            return f"Id:{self.id} Name:{self.name} Adress:{self.adress}  Programs:{self.programs}"
 
-        """
-        Id, Name, Adress, Student
-        Datatyper: String, String, String, [Student]
-        """
-        pass
+nti=School("1","NTI Vetenskapsgymnasiet Solna", "Tomtebodavägen 5", "Science", "Technology" )
+print(nti)
 
-    def add_student(self):
-        """Lägg till en elev"""
-        pass
-
-    def remove_student(self):
-        """Ta bort en specifik elev"""
-        pass
-
-    def remove_class(self):
-        """Ta bort alla elever från en specifik klass"""
-        pass
-
-    def change_address(self):
-        pass
