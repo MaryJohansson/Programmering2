9bad8bb272a0856dd92e606c064dd6b258ee29cb
MaryJohansson <99015835+MaryJohansson@users.noreply.github.com>
2022-02-07 09:52:17 +0100

    hej

--- SkolOrg/School_file.py
+++ SkolOrg/School_file.py
@@ -6,9 +6,12 @@ class School:
         self.adress = adress
         self.programs = programs
 
-        def __str__(self):
-            return f"Id:{self.id} Name:{self.name} Adress:{self.adress}  Programs:{self.programs}"
-
+    def __str__(self):
+        return f"Id:{self.id} Name:{self.name} Adress:{self.adress}  Programs:{self.programs}"
+school=[]
 nti=School("1","NTI Vetenskapsgymnasiet Solna", "Tomtebodavägen 5", "Science", "Technology" )
-print(nti)
+lbs=School("2", "LBS Kreativa Norra", "Tomtebodavägen 5", "Technology", "Aesthete")
+school.append(nti)
+school.append(lbs)
+print(*school,sep="\n")
 
