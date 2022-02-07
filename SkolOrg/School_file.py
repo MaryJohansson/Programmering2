32ecc082c4c5b400f9ecf8d84d78aea3a4b832ab
MaryJohansson <99015835+MaryJohansson@users.noreply.github.com>
2022-02-07 09:18:32 +0100

    hej

--- SkolOrg/School_file.py
+++ SkolOrg/School_file.py
@@ -1,8 +1,18 @@
 class School:
 
-    def __init__(self):
-        ID, Name, Adress, Student
-        Datatyper: String, String, String, [Student]"""
+    def __init__(self,Id, Name, Adress, Student):
+        self.Id = Id
+        self.Name = Name
+        self.Adress = Adress
+        self.Student = Student
+
+        def __str__(self):
+            return f"Id:{self.Id} Name:{self.Name} Adress:{self.Adress}  Student:{self.student}"
+
+        """
+        Id, Name, Adress, Student
+        Datatyper: String, String, String, [Student]
+        """
         pass
 
     def add_student(self):
