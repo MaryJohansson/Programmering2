d3b8a379372107f7ad81571903583d5845c547b6
Ida <99015842+ida1oo@users.noreply.github.com>
2022-02-07 09:09:54 +0100

    org

--- SkolOrg/Org_file.py
+++ SkolOrg/Org_file.py
@@ -1,2 +1,10 @@
-class Games:
+class Organization:
+
     def __init__(self):
+        """Varje organisation ska ha dessa attribut:
+        Id, Namn, Skolor
+        Datatyper: String, String, [School]"""
+        pass
+
+    def add_school(self):
+        pass
