da0f37919e755874d7b74ddc816eb4854e3e7789
Ida <99015842+ida1oo@users.noreply.github.com>
2022-02-08 11:12:26 +0100

    AXELOTT

--- Glosförhör.py
+++ Glosförhör.py
@@ -1,1 +1,11 @@
-print("hey mary")
+with open("glosor.txt", "r") as infil:
+    for rad in infil:
+        rad = rad.strip()
+
+def läsFil (infil):
+    lista = []
+    for rad in infil:
+        rad = rad.strip()
+        lista.append(rad)
+    infil.close()
+    return lista
