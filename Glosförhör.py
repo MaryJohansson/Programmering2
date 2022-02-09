aabe4d9bd777fe8613aca05ce3cba994d3cbe674
Ida <99015842+ida1oo@users.noreply.github.com>
2022-02-09 15:38:12 +0100

    push mary

--- Glosförhör.py
+++ Glosförhör.py
@@ -1,6 +1,13 @@
+dict = {}
 with open("glosor.txt", "r") as infil:
     for rad in infil:
         rad = rad.strip()
+        rad = rad.split(";")
+        dict[rad[0]] = rad[1]
+
+print(dict)
+
+
 
 def läsFil (infil):
     lista = []
@@ -9,5 +16,3 @@ def läsFil (infil):
         lista.append(rad)
     infil.close()
     return lista
-
-print("Hej")
