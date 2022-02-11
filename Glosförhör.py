03fc65f21be51c42090e5b32b536d4e75928d5ba
MaryJohansson <99015835+MaryJohansson@users.noreply.github.com>
2022-02-11 11:14:05 +0100

    hihi

--- Glosförhör.py
+++ Glosförhör.py
@@ -1,11 +1,11 @@
 class Glosor:
 
-    def __init__(self, English, Swedish):
-        self.English = English
-        self.Swedish = Swedish
+    def __init__(self, Engelska, Svenska):
+        self.Engelska = Engelska
+        self.Svenska = Svenska
 
     def __str__(self):
-        return f"English: {self.English} Swedish: {self.Swedish}"
+        return f"Engelska: {self.Engelska} Svenska: {self.Svenska}"
 
 glosor = []
 with open("glosor.txt", "r",encoding="utf8") as file:
@@ -13,5 +13,36 @@ with open("glosor.txt", "r",encoding="utf8") as file:
         row = row.strip("\n")
         row_list = row.split(";")
         glosor.append(Glosor(row_list[0],row_list[1]))
-print(*glosor,sep="\n")
 
+
+choice=[]
+print("Välkommen till vårt glosförhör, detta kan du göra:")
+print("1. Kolla på glosor\n2. Öva på glosor\n3. Lägg till glosa\n4. Ta bort glosa\n")
+
+while choices:=(input("Vad vill du göra?(välj en siffra)")).lower()not in ["1","2","3","4"]:
+    print("Välj vad du vill göra!")
+
+if choices:= "1":
+    print(*glosor, sep="\n")
+
+
+
+
+"""
+    elif choice in ["2" , "look"]:
+       print(*all_pokemon)
+
+=======
+        # Create a new X
+        pass
+
+    elif choice in ["2", "look"]:
+        # Look at all X
+        pass
+>>>>>>> 7497861dcf9b1d574652b621b4e691119adaafa2
+
+    elif choice in ["3", "quit"]:
+        break
+
+    else:
+        print("Not a case")"""
